c80af4cad2c6bc44ab81f9ec696b786261e72c80
Mohammed <manohi1@live.maryville.edu>
2017-11-07 05:29:38 -0800

    Week 1 - [Prog] Powerball Lottery Odds

--- src/LotteryOdds.java
+++ src/LotteryOdds.java
@@ -1,29 +1,41 @@
 import java.math.BigInteger;
 
-public class LotteryOdds {
+public class LotteryOdds 
+{
 
-	public static void main(String[] args) {
-
-		int r;
-		int w = 10; // 69
-		int s = 2; // 5
-		int pb = 26;
-	    r = factorial(w) / (factorial(s) * (factorial(w-s)));  // Superduper magic equation!!
-	    r = r * pb;
+	public static void main(String[] args) 
+	
+	{
+		//declare variables
+		int poolSize = 69; // 69
+		int numberSelected = 5; // 5
+    	BigInteger poolOfBalls = new BigInteger("26");
+    	
+    	//calculate factorial of poolSize, numberSelected and (poolSize - numberSelected) and assign them to BigIntegers
+    	BigInteger factorialPoolSize=factorial(poolSize);
+		BigInteger factorialNumberSelected=factorial(numberSelected);
+		BigInteger factorialOfpoolSizeMinusnumberSelected=factorial(poolSize-numberSelected);
+		
+		//Calculate Superduper magic equation!! i.e. factorial(poolSize) / (factorial(numberSelected) * (factorial(poolSize-numberSelected)))
+		BigInteger oddsOfselectingAllRightNumbers  = factorialPoolSize.divide(factorialNumberSelected.multiply(factorialOfpoolSizeMinusnumberSelected)).multiply(poolOfBalls);
 	    
-	    System.out.println("The odds of winning the PowerBall are 1:" + r);
+	    System.out.println("The odds of winning the PowerBall are 1:" + oddsOfselectingAllRightNumbers);
 	    System.out.println("The PowerBall website says that it's 1:292201338");
 	    
     }
 
-	private static Integer factorial(int n) {
-	    int i = 1;
-	    int f = 1;
-	 
-	    for (int c = 1; c <= n; c++) {
-	      f = f * i++;
-	    }
+	//method to find the factorial of integer 
+	static BigInteger factorial(int number)
+    {
+        // Initialize result
+        BigInteger factorialOfNumber = new BigInteger("1"); // Or BigInteger.ONE
+ 
+        // Multiply f with 2, 3, ...N
+        for (int i = 2; i <= number; i++)
+            factorialOfNumber = factorialOfNumber.multiply(BigInteger.valueOf(i));
+        //the method returns the factorial of the integer passed 
+        return factorialOfNumber;
+    }
 
-		return f;
-	}
+	
 }
